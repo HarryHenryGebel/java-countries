5a3f0ce0650ccdeb867b7d9e57a63e3928f2a55d
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 10:50:00 -0400

    Country getters and setters

--- countrysearch/src/main/java/tech/gebel/countrysearch/models/Country.java
+++ countrysearch/src/main/java/tech/gebel/countrysearch/models/Country.java
@@ -22,4 +22,40 @@ public class Country {
 
     public Country() {
     }
+
+    public long getCountryid() {
+        return countryid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    public long getLandmasskm2() {
+        return landmasskm2;
+    }
+
+    public void setLandmasskm2(long landmasskm2) {
+        this.landmasskm2 = landmasskm2;
+    }
+
+    public int getMedianage() {
+        return medianage;
+    }
+
+    public void setMedianage(int medianage) {
+        this.medianage = medianage;
+    }
 }
