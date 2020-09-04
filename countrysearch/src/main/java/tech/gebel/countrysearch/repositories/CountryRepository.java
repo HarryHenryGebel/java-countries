f0b7572b0f99cda263a116428a8e7b4f1059886d
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 10:53:23 -0400

    Extend CrudRepository

--- countrysearch/src/main/java/tech/gebel/countrysearch/repositories/CountryRepository.java
+++ countrysearch/src/main/java/tech/gebel/countrysearch/repositories/CountryRepository.java
@@ -1,2 +1,7 @@
-package tech.gebel.countrysearch.repositories;public interface CountryRepository {
+package tech.gebel.countrysearch.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import tech.gebel.countrysearch.models.Country;
+
+public interface CountryRepository extends CrudRepository<Country, Long> {
 }
