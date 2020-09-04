b4867f764dbed50b69f41de164eab3b09e658892
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 10:49:49 -0400

    Initialize Country

--- countrysearch/src/main/java/tech/gebel/countrysearch/models/Country.java
+++ countrysearch/src/main/java/tech/gebel/countrysearch/models/Country.java
@@ -1,2 +1,25 @@
-package tech.gebel.countrysearch.models;public class Country {
+package tech.gebel.countrysearch.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long countryid;
+    private String name;
+    private long population;
+    private long landmasskm2;
+    private int medianage;
+
+    public Country(String name, long population, long landmasskm2, int medianage) {
+        this.name = name;
+        this.population = population;
+        this.landmasskm2 = landmasskm2;
+        this.medianage = medianage;
+    }
+
+    public Country() {
+    }
 }
