5d4fe3f90a760e7e77ce3131b7eebc4539cdc0d7
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 11:19:03 -0400

    GetAllCountries

--- countrysearch/src/main/java/tech/gebel/countrysearch/controllers/CountryController.java
+++ countrysearch/src/main/java/tech/gebel/countrysearch/controllers/CountryController.java
@@ -1,9 +1,16 @@
 package tech.gebel.countrysearch.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.gebel.countrysearch.models.Country;
 import tech.gebel.countrysearch.repositories.CountryRepository;
+
 import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class CountryController {
     @SuppressWarnings({"SpringJavaInjectionPointsAutowiringInspection", "SpringJavaAutowiredFieldsWarningInspection"})
@@ -19,5 +26,13 @@ public class CountryController {
             }
         }
         return returnValue;
+    }
+
+    @GetMapping(value = "/names/all", produces = {"application/json"})
+    private ResponseEntity<?> getAllCountries() {
+        List<Country> countries = new ArrayList<>();
+        countryRepository.findAll().iterator().forEachRemaining(countries::add);
+        countries.sort((country1, country2) -> country1.getName().compareToIgnoreCase(country2.getName()));
+        return new ResponseEntity<>(countries, HttpStatus.OK);
     }
 }
