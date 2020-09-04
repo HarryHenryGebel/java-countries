857b2db443d78b798bfaf76efda683b55687f414
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 11:15:36 -0400

    Initialize class

--- countrysearch/src/main/java/tech/gebel/countrysearch/controllers/CountryController.java
+++ countrysearch/src/main/java/tech/gebel/countrysearch/controllers/CountryController.java
@@ -1,2 +1,12 @@
-package tech.gebel.countrysearch.controllers;public class CountryController {
+package tech.gebel.countrysearch.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import tech.gebel.countrysearch.repositories.CountryRepository;
+@RestController
+public class CountryController {
+    @SuppressWarnings({"SpringJavaInjectionPointsAutowiringInspection", "SpringJavaAutowiredFieldsWarningInspection"})
+    @Autowired
+    private CountryRepository countryRepository;
+    private Hashtable<String, Object> response;
 }
