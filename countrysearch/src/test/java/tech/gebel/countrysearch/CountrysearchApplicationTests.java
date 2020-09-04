5ecb5490a34306864e6b3baedad9ec152f6075a3
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 05:02:13 -0400

    Initial commit

---
+++ countrysearch/src/test/java/tech/gebel/countrysearch/CountrysearchApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.gebel.countrysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrysearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
