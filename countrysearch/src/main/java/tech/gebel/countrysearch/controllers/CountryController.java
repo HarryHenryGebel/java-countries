c84a4716576a86fbc4f0ed417fe959ff9efb41ce
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 11:17:16 -0400

    GetCountriesByLetter

--- countrysearch/src/main/java/tech/gebel/countrysearch/controllers/CountryController.java
+++ countrysearch/src/main/java/tech/gebel/countrysearch/controllers/CountryController.java
@@ -3,10 +3,21 @@ package tech.gebel.countrysearch.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import tech.gebel.countrysearch.repositories.CountryRepository;
+import java.util.ArrayList;
 @RestController
 public class CountryController {
     @SuppressWarnings({"SpringJavaInjectionPointsAutowiringInspection", "SpringJavaAutowiredFieldsWarningInspection"})
     @Autowired
     private CountryRepository countryRepository;
     private Hashtable<String, Object> response;
+
+    private ArrayList<Country> getCountriesByLetter(ArrayList<Country> listToTest, CountryFilter test) {
+        ArrayList<Country> returnValue = new ArrayList<>();
+        for (Country country : listToTest) {
+            if (test.test(country)) {
+                returnValue.add(country);
+            }
+        }
+        return returnValue;
+    }
 }
